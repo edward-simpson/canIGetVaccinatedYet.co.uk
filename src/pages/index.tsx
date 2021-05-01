34d125c4134e26d03a86dc66635ba65e9d2f69d5
Edward Simpson <edwardjamessimpson@gmail.com>
2021-05-01 12:34:20 +0100

    DD#0000: fix: more disclamer :eyeroll:

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,11 +13,13 @@ const IndexPage = () => (
     <h2>Is the current bottom age limit of vaccination* in England.</h2>
     <p>
       *This does not take into account people with preexisting conditions, care
-      workers, etc, etc. Check the{" "}
+      workers, etc, etc. to see other conditions. This is purely meant as a
+      guide, if you feel you should be getting the vaccine you should probably
+      ceck the{" "}
       <Link to="https://www.nhs.uk/conditions/coronavirus-covid-19/coronavirus-vaccination/book-coronavirus-vaccination/">
         gov site
       </Link>{" "}
-      to see other conditions.
+      and/or get in contact with your GP.
     </p>
   </Layout>
 );
